c82f248cf8b2bec3f81eb8adf9f9bf0df00ec827
Christian Esken <Christian.Esken@trivago.com>
2017-03-07 18:00:07 +0100

    Make QueuedMessage.isTimedOut() better readable

--- src/java/org/apache/cassandra/net/OutboundTcpConnection.java
+++ src/java/org/apache/cassandra/net/OutboundTcpConnection.java
@@ -640,7 +640,7 @@ public class OutboundTcpConnection extends Thread
         boolean isTimedOut(long nowNanos)
         {
             long messageTimeoutNanos = TimeUnit.MILLISECONDS.toNanos(message.getTimeout());
-            return droppable && timestampNanos - (nowNanos - messageTimeoutNanos) < 0;
+            return droppable && nowNanos - timestampNanos  > messageTimeoutNanos;
         }
 
         boolean shouldRetry()
