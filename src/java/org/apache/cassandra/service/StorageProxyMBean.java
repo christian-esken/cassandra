adaff6ae61532f89cf96ae45e115c8367ad137b1
Christian Esken <Christian.Esken@trivago.com>
2017-03-17 11:02:41 +0100

    Preparing soure tree for creating patches
    Whitespace cleanup
    Move from Integer to int for OtcBacklogExpirationInterval. Re-add that change, as it looks it got lost during rebase operation.

--- src/java/org/apache/cassandra/service/StorageProxyMBean.java
+++ src/java/org/apache/cassandra/service/StorageProxyMBean.java
@@ -57,9 +57,9 @@ public interface StorageProxyMBean
     public long getReadRepairAttempted();
     public long getReadRepairRepairedBlocking();
     public long getReadRepairRepairedBackground();
-    
-    public Integer getOtcBacklogExpirationInterval();
-    public void setOtcBacklogExpirationInterval(Integer intervalInMillis);
+
+    public int getOtcBacklogExpirationInterval();
+    public void setOtcBacklogExpirationInterval(int intervalInMillis);
 
     /** Returns each live node's schema version */
     public Map<String, List<String>> getSchemaVersions();
